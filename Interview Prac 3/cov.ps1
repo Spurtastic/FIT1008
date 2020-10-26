97cb6b0a5754c71d1f11df4bd9eb21ba8fa52956
Ashwin Sarith <asar0015@student.monash.edu>
2020-10-26 20:52:48 +0800

    made the menus

--- Interview Prac 3/cov.ps1
+++
@@ -1,7 +0,0 @@
-# coverage run dictionary.py
-# coverage report -m
-# coverage html 
-
-git add -A
-git commit -m "continuous testing view"
-git push 
--- Interview Prac 3/dictionary.py
+++ Interview Prac 3/dictionary.py
@@ -4,16 +4,81 @@ import timeit
 
 
 class Statistics:
-    # TODO
-    pass
+
+   pass
+
+    
 
 
 class Dictionary:
+    # returns None
+    def __init__(self, hash_base: int, table_size: int):
+        self.base = hash_base
+        self.table = [None] * table_size
+
+    # returns integer
+    def load_dictionary(self, filename: str, time_limit: int = None):
+        
+        pass
+
+    # returns None
+    def add_word(self, word: str):
+        pass
+
+    #returns None
+    def find_word(self, word: str):
+        pass
+
+    #returns None
+    def delete_word(self, word: str):
+        pass
+
+    #returns None
+    def menu(self):
+        
+        usr_input = True
+        while usr_input:
+            print("""
+            1. Read File
+            2. Delete word
+            3. Find Word
+            4. Add Word
+            5. Quit""")
+            input_recieved = int(input("Input the appropriate number for your required command: "))
+            if input_recieved == 5:
+                usr_input = False
+                res="You quit"
+                return res
+
+            elif input_recieved == 1:
+                res = "File read"
+                print(res)
+            
+            elif input_recieved == 2:
+                res = "Word deleted"
+                print(res)
+
+            elif input_recieved == 3:
+                res = "Word found in file"
+                print(res)
+
+            elif input_recieved == 4:
+                res = "Word added"
+                print(res)
+                
+            else:
+                res="invalid input try again"
+                print(res)
+
+
+
     # TODO
-    pass
 
 
 if __name__ == '__main__':
     # TODO
-    pass
+    dict = Dictionary(3,2)
+    print(dict.menu())
+
+
 
